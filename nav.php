07d240fed92162ebb9d6b545f7189d62bc0fee1c
Logan Darling <logandarling99@gmail.com>
2020-12-28 21:47:38 -0500

    Initial Setup

---
+++ nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Mera Handymen</title>
+  <meta name="description" content="Mera Handymen and Cleaning">
+  <meta name="author" content="Logan Darling">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+
+</head>
+<body>
+  
+
+  <nav class="navbar navbar-dark bg-dark navbar-expand-md fixed-top" style="background:transparent">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="samplelogo.jpg" alt="" width="200" height="50">
+      </a>
+    </div>
+  </nav>
+</body>
+</html>
