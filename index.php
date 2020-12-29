1f6cf5f87926fa8e1c74935bb8cb1824b3aa28b0
nsanchez2017 <nsanchez2017@my.fit.edu>
2020-12-29 00:22:06 -0500

    updated logo and fix right align on nav bar

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="Mera Handymen and Cleaning">
   <meta name="author" content="Logan Darling">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link href="styles.css" rel="stylesheet">
 
 
