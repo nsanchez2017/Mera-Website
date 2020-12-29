b708f43f0a22385b94d86d806d79e76823d0b91d
nsanchez2017 <nsanchez2017@my.fit.edu>
2020-12-28 23:41:05 -0500

    update nav with new opacity and buttons

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-      
-        <title>Mera Handymen and Cleaning</title>
-        <meta name="description" content="Mera Handymen and Cleaning">
-        <meta name="author" content="Logan Darling">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        
-      
-      </head>
-      <body>
-        <?php include "nav.php"; ?>
-      </body>
+
+<head>
+  <meta charset="utf-8">
+
+  <title>Mera Handymen and Cleaning</title>
+  <meta name="description" content="Mera Handymen and Cleaning">
+  <meta name="author" content="Logan Darling">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="styles.css" rel="stylesheet">
+
+
+</head>
+
+<header class="home-image">
+
+</header>
+<body>
+  <?php include "nav.php"; ?>
+
+
+</body>
+
 </html>
--- nav.php
+++ nav.php
@@ -15,27 +15,19 @@
 </head>
 
 <body>
-  <header class="home-image">
-
-  </header>
-  <nav class="navbar navbar-expand-sm fixed-top navbar-light">
-    <div class="container">
-      <a class="navbar-brand" href="#">Brand</a>
+  <nav class="navbar navbar-expand-sm fixed-top navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">MERA</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbar1">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">About Us</a>
           </li>
         </ul>
       </div>
