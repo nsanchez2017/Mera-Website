23181e6504374bc3c5467b441ce7061166b3d36b
nsanchez2017 <nsanchez2017@my.fit.edu>
2021-01-03 20:56:55 -0500

    change bootstrap and fixed script

--- about.php
+++ about.php
@@ -6,22 +6,41 @@
 
   <title>Mera Handymen and Cleaning</title>
   <meta name="description" content="Mera Handymen and Cleaning">
-  <meta name="author" content="Nicangel Sanchez">
+  <meta name="author" content="Nicangel Sanchez"> 
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <link href="styles.css" rel="stylesheet">
 
 
 </head>
-<body>
-  <?php include "nav.php"?>
+<header class="about-image">
   <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h3 class = "text-center">About Us</h3>
+    <div class="row justify-content-center">
+      <div class="col-10 text-center justify-content-center">
+        <h1 class="text-center home-image-text display-2">About Us</h1><br>
+      </div>
+      <div class="col-6 text-center">
+        <h4 class="text-center home-image-text text-wrap">We are proud of delivering the top-quality repair and maintenance services in Brevard.</h4>
       </div>
     </div>
   </div>
+</header>
+
+<body>
+  <?php include "nav.php"?>
+  <div class="container pt-4">
+    <div class="row">
+      <div class="col-12">
+        <h3 class = "text-center dark-green-color">Business Hours</h3>
+      </div>
+    </div>
+  </div>
+  &nbsp;
 </body>
+
+<footer>
+  <?php include "footer.php"?>
+</footer>
+
 
 </html>
